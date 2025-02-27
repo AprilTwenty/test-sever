112212dc65f4dcb76f4a57d57072564cec016e03
spxhub1864 <spxhub1864@th.corp.seagroup.com>
2025-02-27 15:05:46 +0700

    add data for get request

---
+++ app.mjs
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.get("/test", (req, res) => {
+    return res.json(data);
+});
+
+app.listen(port, () => {
+    console.log("server use port at" + port);
+});
+
+const data = {
+    name : "John",
+    age : "20"
+};
